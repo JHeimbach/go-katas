eed257f0f6f28ee70fd320914e86024ff4e13f93
johannes Heimbach <heimbachjohannes@gmail.com>
2019-09-27 18:49:50 +0200

    helper programm to create kata files

---
+++ create_kata.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("no name given")
+	}
+	var name = os.Args[1]
+
+	folderPath := filepath.Join(".", name)
+	err := os.Mkdir(folderPath, os.ModePerm)
+
+	if err != nil {
+		log.Fatalf("could not create dir %q", folderPath)
+	}
+
+	createGoFile("kata.go", folderPath)
+	createGoFile("kata_test.go", folderPath)
+}
+
+func createGoFile(name string, folderPath string) {
+	filename := filepath.Join(folderPath, name)
+	err := ioutil.WriteFile(filename, []byte("package kata \n\n"), os.ModePerm)
+	if err != nil {
+		log.Fatalf("could not create file %q", filename)
+	}
+}
